0c6069fe55a30c219858867cd1918f31ea8e9638
rehanshah17 <rehans0906@gmail.com>
2025-01-01 17:20:40 -0500

    added radars and collision detection

--- main.py
+++ main.py
@@ -22,14 +22,36 @@ class Car(pygame.sprite.Sprite):
         self.angle = 0
         self.rotation_vel = 5
         self.direction = 0
+        self.alive = True
 
     def update(self):
         self.drive()
         self.rotate()
+        for radar_angle in (-60,-30,0,30,60):
+            self.radar(radar_angle)
+        self.collision()
     
     def drive(self):
         if self.drive_state:
             self.rect.center += self.vel_vector * 6
+    
+    def collision(self):
+        length = 40
+        # Set collision points on right and left headlights
+        collision_point_right = [int(self.rect.center[0] + math.cos(math.radians(self.angle + 18)) * length),
+                                  int(self.rect.center[1] - math.sin(math.radians(self.angle + 18)) * length)]
+        collision_point_left = [int(self.rect.center[0] + math.cos(math.radians(self.angle - 18)) * length),
+                                  int(self.rect.center[1] - math.sin(math.radians(self.angle - 18)) * length)]
+        
+        # Die on Collisions
+        if SCREEN.get_at(collision_point_right) == pygame.Color(2,105,31,255) \
+            or SCREEN.get_at(collision_point_left)  == pygame.Color(2,105,31,255):
+            self.alive = False
+        
+        # Draw collision points
+
+        pygame.draw.circle(SCREEN, (0,255,255,0), collision_point_right,4)
+        pygame.draw.circle(SCREEN,(0,255,255,0),collision_point_left,4)
     
     def rotate(self):
         if self.direction == 1:
@@ -40,6 +62,21 @@ class Car(pygame.sprite.Sprite):
             self.vel_vector.rotate_ip(-self.rotation_vel)
         self.image = pygame.transform.rotozoom(self.original_image,self.angle,0.1)
         self.rect = self.image.get_rect(center=self.rect.center)
+    
+    def radar(self,radar_angle):
+        length = 0
+        x = int(self.rect.center[0])
+        y = int(self.rect.center[1])
+
+        while not SCREEN.get_at((x,y)) == pygame.Color(2,105,31,255) and length < 200:
+            length += 1
+            x = int(self.rect.center[0] + math.cos(math.radians(self.angle + radar_angle))  * length)
+            y = int(self.rect.center[1] - math.sin(math.radians(self.angle + radar_angle))  * length)
+        
+        # Draw radar
+        pygame.draw.line(SCREEN, (255,255,255,255), self.rect.center,(x,y),1)
+        pygame.draw.circle(SCREEN,(0,255,0,0),(x,y),3)
+        
 
 car = pygame.sprite.GroupSingle(Car()) #container that holds a single sprite image: Car
 
